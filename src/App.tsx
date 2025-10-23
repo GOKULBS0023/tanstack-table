b5c086630239791fda20baff3f46d71138915131
Gokulbharathi S <gokulbs0023@gmail.com>
2025-10-23 20:35:44 +0530

    feat: integrate Chakra UI and create TaskTable component
    - Added Chakra UI dependencies to package.json.
    - Updated App component to use Chakra UI for layout and styling.
    - Created TaskTable component for displaying tasks.
    - Added data structure for task statuses and sample tasks.
    - Implemented global styles and theme configuration for Chakra UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import TaskTable from "./components/TaskTable";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box maxW={1000} mx="auto" p={6} pt={24} fontSize="sm">
+        <Heading mb={10}>TanStack Table</Heading>
+        <TaskTable />
+      </Box>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/TaskTable.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const TaskTable = () => {
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
+};
+
+export default TaskTable;
